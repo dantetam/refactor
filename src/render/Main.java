e05f0325f3a668d6bd8c10bb2e7abcf65a81eeea
dantetam <dante.tam1@gmail.com>
2014-11-14 19:39:08 -0800

    First commit

---
+++ src/render/Main.java
@@ -0,0 +1,17 @@
+package render;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public void setup()
+	{
+		size(1600,900);
+	}
+	
+	public void draw()
+	{
+		background(255);
+	}
+	
+}
